54cb69e33fa4d7265d11ff43d3bc29d65e25b41b
eugen <eugen@eugen.de>
2017-01-20 16:35:01 +0100

    update

--- caffe/examples/ctpn/CTPN.hpp
+++ caffe/examples/ctpn/CTPN.hpp
@@ -227,7 +227,7 @@ public:
 	{
 		for (int i=0; i<rois_blob->shape()[0]; i++)
 		{
-			cv::Rect r(rois_blob->data_at(i,0,0,0)+0.5 //x
+			cv::Rect r(rois_blob->data_at(i,0,0,0)+0.5, //x
 				rois_blob->data_at(i, 1,0,0)+0.5, //y
 				rois_blob->data_at(i, 2,0,0)-rois_blob->data_at(i, 0,0,0), //width
 				rois_blob->data_at(i, 3,0,0)-rois_blob->data_at(i, 1,0,0) //height
